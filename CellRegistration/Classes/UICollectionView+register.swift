b64b68da4e75c8b9a37e2497155f02c4ba798a15
Grzegorz Krukowski <grkrukowski@gmail.com>
2017-03-20 17:48:04 +0100

    1.3.0

--- CellRegistration/Classes/UICollectionView+register.swift
+++ CellRegistration/Classes/UICollectionView+register.swift
@@ -12,7 +12,7 @@ private struct AssociatedObjectKey {
     static var registeredCells = "registeredCells"
 }
 
-extension UITableView {
+extension UICollectionView {
     private var registeredCells: Set<String> {
         get {
             if objc_getAssociatedObject(self, &AssociatedObjectKey.registeredCells) as? Set<String> == nil {
@@ -26,24 +26,24 @@ extension UITableView {
         }
     }
     
-    public func register<T: UITableViewCell>(_: T.Type) where T: ReusableView {
+    private func register<T: UICollectionViewCell>(_: T.Type) where T: ReusableView {
         let bundle = Bundle(for: T.self)
         
         if bundle.path(forResource: T.reuseIdentifier, ofType: "nib") != nil {
             let nib = UINib(nibName: T.reuseIdentifier, bundle: bundle)
-            register(nib, forCellReuseIdentifier: T.reuseIdentifier)
+            register(nib, forCellWithReuseIdentifier: T.reuseIdentifier)
         } else {
-            register(T.self, forCellReuseIdentifier: T.reuseIdentifier)
+            register(T.self, forCellWithReuseIdentifier: T.reuseIdentifier)
         }
     }
     
-    public func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T where T: ReusableView {
+    public func dequeueReusableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T where T: ReusableView {
         if self.registeredCells.contains(T.reuseIdentifier) == false {
             self.registeredCells.insert(T.reuseIdentifier)
             self.register(T.self)
         }
         
-        guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
             fatalError("Error dequeuing cell with reuse identifier: \(T.reuseIdentifier)")
         }
         
--- CellRegistration/Classes/UITableView+register.swift
+++ CellRegistration/Classes/UITableView+register.swift
@@ -12,7 +12,7 @@ private struct AssociatedObjectKey {
     static var registeredCells = "registeredCells"
 }
 
-extension UICollectionView {
+extension UITableView {
     private var registeredCells: Set<String> {
         get {
             if objc_getAssociatedObject(self, &AssociatedObjectKey.registeredCells) as? Set<String> == nil {
@@ -26,14 +26,14 @@ extension UICollectionView {
         }
     }
 
-    public func register<T: UITableViewCell>(_: T.Type) where T: ReusableView {
+    private func register<T: UITableViewCell>(_: T.Type) where T: ReusableView {
         let bundle = Bundle(for: T.self)
 
         if bundle.path(forResource: T.reuseIdentifier, ofType: "nib") != nil {
             let nib = UINib(nibName: T.reuseIdentifier, bundle: bundle)
-            register(nib, forCellWithReuseIdentifier: T.reuseIdentifier)
+            register(nib, forCellReuseIdentifier: T.reuseIdentifier)
         } else {
-            register(T.self, forCellWithReuseIdentifier: T.reuseIdentifier)
+            register(T.self, forCellReuseIdentifier: T.reuseIdentifier)
         }
     }
 
@@ -43,7 +43,7 @@ extension UICollectionView {
             self.register(T.self)
         }
 
-        guard let cell = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+        guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
             fatalError("Error dequeuing cell with reuse identifier: \(T.reuseIdentifier)")
         }
 
